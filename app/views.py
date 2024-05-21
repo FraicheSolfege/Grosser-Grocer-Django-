7710053c2685e3161b4a6e80b16d2cb4ed42d8db
FraicheSolfege <littlenathan68@yahoo.com>
2024-05-21 15:24:16 -0500

    might have to roll back

--- app/views.py
+++ app/views.py
@@ -68,6 +68,24 @@ def register(request):
     context = {'form': form}
     return render(request, 'register.html' , context)
 
+@unauthenticated_user
+def admin_register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username =form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='admin')
+            user.groups.add(group)
+
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+        
+    context = {'form': form}
+    return render(request, 'admin_register.html' , context)
+
 
 
 
